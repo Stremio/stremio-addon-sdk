68a654a979d47a416d29d0b1a674816a410c30c5
Ivo Georgiev <ivo@linvo.org>
2018-03-25 20:09:22 +0300

    http version

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 A NodeJS SDK for making and publishing Stremio add-ons
 
+This can publish an add-on via HTTP(s) or IPFS
 
+
+## Documentation
+
+**TODO**
---
+++ index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const express = require('express')
+
+module.exports = function Addon(manifest) {
+	const addonHTTP = express()
+
+	const manifestBuf = new Buffer(JSON.stringify(manifest))
+	addonHTTP.get('/manifest.json', function (req, res) {
+		res.setHeader('Content-Type', 'application/json; charset=utf-8')
+		res.send(manifestBuf)
+	})
+
+	this.defineResource = function(resource, handler) {
+		// WARNING: someone can pass a resource of ':something'
+		addonHTTP.get('/'+resource+'/:type/:id.json', function(req, res) {
+			handler(req.params.type, req.params.id, function(err, resp) {
+				if (err) {
+					console.error(err)
+					res.status(500).send({ err: 'handler error' })
+				}
+
+				res.send(resp)
+			})
+		})
+	}
+
+	this.defineStreams = this.defineResource.bind(this, 'stream')
+	this.defineCatalog = this.defineResource.bind(this, 'catalog')
+	this.defineSubtitles = this.defineResource.bind(this, 'subtitles')
+
+	this.http = addonHTTP
+}
+
