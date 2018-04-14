a8405fab571a697c81bab176d47ff6a7d580d45e
Ivo Georgiev <ivo@strem.io>
2018-04-14 16:49:24 +0300

    basic test: add test of extra args

--- test/basic.js
+++ test/basic.js
@@ -16,10 +16,15 @@ const manifest = {
 	types: ['movie'],
 }
 
-tape('create an add-on and connect to it', function(t) {
-	const addon = new addonSDK(manifest)
+let addon
+let addonUrl
+let addonServer
 
-	let addonClient
+let addonClient
+
+
+tape('create an add-on and expose on HTTP', function(t) {
+	addon = new addonSDK(manifest)
 
 	addon.run(function(err, h) {
 		// This executes first
@@ -28,24 +33,62 @@ tape('create an add-on and connect to it', function(t) {
 		t.ok(h.url, 'has url')
 		t.ok(h.url.endsWith('manifest.json'), 'url ends with manifest.json')
 
-		AddonClient.detectFromURL(h.url)
-		.then(function(resp) {
-			t.ok(resp.addon, 'has addon')
+		t.ok(h.server, 'has h.server')
 
-			return resp.addon.get('stream', 'channel', '11')
-		})
-		.catch(function(err) {
-			t.error(err, 'error on addonclient')
-			t.end()
-		})
+		addonUrl = h.url
+		addonServer = h.server
+
+		t.end()
+	})
+})
+
+tape('initialize an add-on client for the add-on', function(t) {
+	AddonClient.detectFromURL(addonUrl)
+	.then(function(resp) {
+		t.ok(resp.addon, 'has addon')
+
+		t.ok(resp.addon.manifest, 'has manifest')
+		// NOTE: this is an important design principle - immutability on the manifest object
+		t.deepEquals(resp.addon.manifest, manifest, 'addon.manifest is the same as manifest')
+
+		addonClient = resp.addon
+		t.end()
+	})
+	.catch(function(err) {
+		t.error(err, 'error on addonclient')
+		t.end()
+	})
+})
+
+tape('define a stream handler on the add-on and test it', function(t) {
+	addonClient.get('stream', 'channel', '11')
+	.catch(function(err) {
+		t.error(err, 'error on addonclient stream request')
+		t.end()
 	})
 
-
+	// NOTE: this also tests the case where .defineStreamHandler is invoked after .run()
 	addon.defineStreamHandler(function(args, cb) {
-		// This executes second
 		t.equals(args.type, 'channel', 'args.type is right')
 		t.equals(args.id, '11', 'args.id is right')
 		t.deepEquals(args.extra, { }, 'args.extra is empty')
+
+		t.end()
+	})
+})
+
+// @WARNING: we should throw the second time we call defineStreamHandler (same goes for define*Handler)
+tape('define a handler on the add-on and test it, with extra args', function(t) {
+	addonClient.get('catalog', 'movie', 'top', { search: 'the office' })
+	.catch(function(err) {
+		t.error(err, 'error on addonclient stream request')
+		t.end()
+	})
+
+	addon.defineCatalogHandler(function(args, cb) {
+		t.equals(args.type, 'movie', 'args.type is right')
+		t.equals(args.id, 'top', 'args.id is right')
+		t.deepEquals(args.extra, { search: 'the office' }, 'args.extra is right')
 
 		t.end()
 
