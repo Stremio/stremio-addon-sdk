513d9ddb41b3e000f466054c8d1ec542d0b6c986
Ivo Georgiev <ivo@strem.io>
2018-08-10 15:59:18 +0100

    add .runHTTPWithOptions function

--- index.js
+++ index.js
@@ -70,10 +70,14 @@ module.exports = function Addon(manifest) {
 
 	// .run - starts the add-on listening on some port
 	this.run = function(cb) {
+		this.runHTTPWithOptions({ port: process.env.PORT || null }, cb)
+	}
+
+	this.runHTTPWithOptions = function(options, cb) {
 		var addonHTTPApp = express()
 		addonHTTPApp.use('/', addonHTTP)
 		var server = http.createServer(addonHTTPApp)
-		server.listen(process.env.PORT || null, function() {
+		server.listen(options.port, function() {
 			var url = 'http://127.0.0.1:'+server.address().port+'/manifest.json'
 			console.log('HTTP addon accessible at:', url)
 			if (cb) cb(null,  { server: server, url: url })
