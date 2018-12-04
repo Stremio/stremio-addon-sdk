cef281eccf3b39419387d01190110ee339644bdd
Jaruba Firanelli <jaruba@Jarubas-MacBook-Pro.local>
2018-12-04 17:44:29 +0200

    Create Homepage only when `publicToWeb` is used

--- static/js/helpers.js
+++ static/js/helpers.js
@@ -1,9 +1,5 @@
 module.exports = {
    stylize: function (string) {
       return string.charAt(0).toUpperCase() + string.slice(1) + (string !=='series' ? 's,' : ',');
-   },
-   toggle: function (string) {
-      if (string) return "enabled"
-      else return "disabled"
    }
 }
