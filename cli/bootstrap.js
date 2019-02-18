3e6c8309bd0256acec1264f9ae3be78d489ea9fe
Ivo Georgiev <ivo@strem.io>
2019-02-18 16:31:34 +0200

    bootstapper progress

--- cli/bootstrap.js
+++ cli/bootstrap.js
@@ -3,15 +3,35 @@
 const [,, ...args] = process.argv
 const dir = args.find(arg => !arg.startsWith('--'))
 
+const chalk = require('chalk')
 const fs = require('fs')
 const {promisify} = require('util')
+const mkdirp = promisify(require('mkdirp'))
+const yesno = require('yesno')
 
 if (typeof(dir) !== 'string') usage()
 if (fs.existsSync(dir)) usage({exists: true})
 
 
+createAddon()
+.then(() => console.log(chalk.green(`BOOTSTRAPPER: addon created!`)))
+
+async function createAddon() {
+	await mkdirp(dir)
+	
+	console.log(chalk.green(`BOOTSTRAPPER: directory created: ${dir}`))
+
+	const hasCatalogs = await yesno.askAsync('Does your addon provide catalogs?', true)
+	//const hasMetas = await yesno.askAsync('Does your addon provide metas?', true)
+	//const hasStreams = await yesno.askAsync('Does your addon provide streams?', true)
+
+	console.log('done')
+	// @TODO types and id prefixes
+	// @TODO subtitles
+}
+
 function usage({exists} = {}) {
-	if (exists) console.log(`Output directory already exists!`)
+	if (exists) console.log(chalk.red(`Output directory already exists!`))
 	else console.log(`Usage: ${process.argv[1]} {OUTPUT DIRECTORY}`)
 	process.exit(1)
 }
