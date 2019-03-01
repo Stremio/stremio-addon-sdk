9213640f474e4a12ab9515b74234b7dbcef4dbf4
Ivo Georgiev <ivo@strem.io>
2019-03-01 00:15:16 +0000

    cli/bootstrap: fix typo

--- cli/bootstrap.js
+++ cli/bootstrap.js
@@ -95,7 +95,7 @@ const addonInterface = require('./addon')
 serveHTTP(addonInterface, { port: 7778 })
 
 // when you've deployed your addon, un-comment this line
-// publishToCentral('https://my-addon.awesoem/manifest.json')
+// publishToCentral('https://my-addon.awesome/manifest.json')
 // for more information on deploying, see: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/deploying.md
 `
 
