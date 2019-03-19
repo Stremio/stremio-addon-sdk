e4d820ed98b48d536e21db488213d19dd0ad4a43
Jaruba Firanelli <branza.alex@gmail.com>
2019-03-19 12:51:49 +0200

    Fix Links to Deployment Docs

--- cli/bootstrap.js
+++ cli/bootstrap.js
@@ -117,7 +117,7 @@ serveHTTP(addonInterface, { port: ${Math.floor(Math.random() * 16383) + 49152} }
 
 // when you've deployed your addon, un-comment this line
 // publishToCentral("https://my-addon.awesome/manifest.json")
-// for more information on deploying, see: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/deploying.md
+// for more information on deploying, see: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/deploying/README.md
 `
 
 const headerTmpl = (manifest) => `const { addonBuilder } = require("stremio-addon-sdk")
