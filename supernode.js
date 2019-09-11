9da17ae8f9320d09d552d0bcdb40bcb851110370
Ivo Georgiev <ivo@strem.io>
2019-09-11 20:19:17 +0300

    supernode: persistency

--- supernode.js
+++ supernode.js
@@ -1,7 +1,13 @@
 const WebSocket = require('ws')
 const ipfsClient = require('ipfs-http-client')
-const wss = new WebSocket.Server({ port: 14001 })
-wss.on('connection', onConnection)
+
+// @TODO: this is duplicated with publish.js, fix it
+const IPFS_WRITE_OPTS = {
+	create: true,
+	parents: true,
+	truncate: true
+}
+const IPFS_MSG_PATH = '/msgs'
 
 const byIdentifier = new Map()
 
@@ -9,18 +15,30 @@ function onConnection(ws) {
 	ws.on('message', data => {
 		let msg
 		try { msg = JSON.parse(data) } catch(e) {
-			console.error(err)
+			console.error(e)
 		}
 		// @TODO more checks
-		if (msg) onMessage(msg)
+		if (msg) onMessage(msg).catch(e => console.error(e))
 	})
 	//ws.send(JSON.stringify());
 }
 
 // @TODO pin
-function onMessage(msg) {
+async function onMessage(msg) {
 	console.log(msg)
 	if (msg.type === 'Publish') {
+		byIdentifier.set(msg.identifier, msg.hash)
+		await ipfs.files.write(`${IPFS_MSG_PATH}/${msg.identifier}`, Buffer.from(JSON.stringify(msg)), IPFS_WRITE_OPTS)
+	}
+}
+
+async function readCachedMsgs() {
+	const files = await ipfs.files.ls(`/${IPFS_MSG_PATH}`)
+	// @TODO: warning: this doesn't consider whether the contents are files or not
+	const buffers = await Promise.all(files.map(f => ipfs.files.read(`/${IPFS_MSG_PATH}/${f.name}`)))
+	for (buf of buffers) {
+		// @TODO: reuse onMessage, but without the write
+		const msg = JSON.parse(buf.toString())
 		byIdentifier.set(msg.identifier, msg.hash)
 	}
 }
@@ -47,4 +65,16 @@ app.use('/:identifier', async function(req, res, next) {
 		res.sendStatus(404)
 	}
 })
-app.listen(3006)
+
+// Initialize
+async function init() {
+	// replay previous Publish messages
+	await readCachedMsgs()
+
+	const wss = new WebSocket.Server({ port: 14001 })
+	wss.on('connection', onConnection)
+
+	app.listen(3006)
+}
+
+init().catch(e => console.error(e))
