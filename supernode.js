52b04e1630a2530e82d7e6075d3a62778575f12b
Ivo Georgiev <ivo@strem.io>
2019-09-14 20:08:01 +0300

    request parsing

--- supernode.js
+++ supernode.js
@@ -2,6 +2,7 @@ const WebSocket = require('ws')
 const ipfsClient = require('ipfs-http-client')
 const HDKey = require('hdkey')
 const crypto = require('crypto')
+const qs = require('querystring')
 
 const { IPFS_WRITE_OPTS, IPFS_MSG_PATH } = require('./p2p')
 
@@ -21,7 +22,7 @@ function onConnection(ws) {
 			// @TODO more checks
 			if (msg) onMessage(xpub, msg).catch(e => console.error(e))
 		} catch(e) {
-			// catches the parse
+			// catches the parse and the msg verification
 			// @TODO better way to do this
 			console.error(e)
 		}
@@ -33,7 +34,6 @@ function onConnection(ws) {
 async function onMessage(xpub, msg) {
 	if (msg.type === 'Publish') {
 		const identifier = `${xpub.slice(4, 16)}.${msg.identifier}`
-		//console.log(identifier)
 		byIdentifier.set(identifier, msg.hash)
 		await ipfs.files.write(`${IPFS_MSG_PATH}/${identifier}`, Buffer.from(JSON.stringify(msg)), IPFS_WRITE_OPTS)
 	}
@@ -42,11 +42,11 @@ async function onMessage(xpub, msg) {
 async function readCachedMsgs() {
 	const files = await ipfs.files.ls(`/${IPFS_MSG_PATH}`)
 	// @TODO: warning: this doesn't consider whether the contents are files or not
-	const buffers = await Promise.all(files.map(f => ipfs.files.read(`/${IPFS_MSG_PATH}/${f.name}`)))
-	for (let buf of buffers) {
-		// @TODO: reuse onMessage, but without the write
+	for (let file of files) {
+		const identifier = file.name
+		const buf = await ipfs.files.read(`/${IPFS_MSG_PATH}/${identifier}`)
 		const msg = JSON.parse(buf.toString())
-		byIdentifier.set(msg.identifier, msg.hash)
+		byIdentifier.set(identifier, msg.hash)
 	}
 }
 
@@ -55,29 +55,59 @@ async function readCachedMsgs() {
 const express = require('express')
 const app = express()
 const ipfs = ipfsClient('localhost', '5001', { protocol: 'http' })
+
 app.use('/:identifier', async function(req, res) {
+	res.setHeader('content-type', 'application/json')
 	const hash = byIdentifier.get(req.params.identifier.trim())
 	if (hash) {
 		const path = `/ipfs/${hash}${req.url}`
-		res.setHeader('content-type', 'application/json')
 		ipfs.catReadableStream(path)
 			.on('error', e => {
 				if (e.statusCode === 500 && e.message.startsWith('no link named'))
-					res.status(404).json({ error: 'Not found' })
+					handleNotFound(req, res, () => res.status(404).json({ err: 'not found' }))
 				else
 					throw e
 			})
 			.pipe(res)
 	} else {
-		res.sendStatus(404)
+		res.status(404).json({ err: 'no addon with that identifier' })
 	}
 })
+
+// won't work wit hgetrouter
+// we have to respond with either origin down
+// or with a gateway timeout
+// or with the response
+function handleNotFound(req, res) {
+	// @TODO validate the request, if it's valid then we'll do a dynamic request
+	// upon 404
+	const parsed = parseRequest(req.url)
+	if (!parsed) {
+		res.status(400).json({ err: 'failed to parse request' })
+		return
+	}
+
+	console.log(parsed)
+	res.status(404).json({ error: 'Not found' })
+}
+
+function parseRequest(url) {
+	if (!url.startsWith('/')) return null
+	if (!url.endsWith('.json')) return null
+	const segments = url.slice(1, -5).split('/')
+	if (segments.length === 3) return segments
+	if (segments.length === 4) return [ ...segments.slice(0, 3), qs.parse(segments[3]) ]
+	return null
+}
+
 
 // Initialize
 async function init() {
 	// replay previous Publish messages
 	await readCachedMsgs()
 
+	// and start listening on the WebSocket
+	// @TODO ports to not be hardcoded
 	const wss = new WebSocket.Server({ port: 14001 })
 	wss.on('connection', onConnection)
 
