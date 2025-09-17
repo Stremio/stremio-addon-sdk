909521cb6d547fbaf6068a16f3de0bff1fbc822d
Sleeyax <yourd3veloper@gmail.com>
2025-09-17 18:44:44 +0200

    document example on how to use the `getRouter` function

--- src/getRouter.d.ts
+++ src/getRouter.d.ts
@@ -1,7 +1,30 @@
 import { AddonInterface } from './types';
 
 /**
- * Turns the addonInterface into an express router that serves the addon according to the protocol and a landing page on the root (/).
+ * Turns the addon interface into an express router that serves the addon according to the protocol and a landing page on the root (/).
+ * @example
+ * ```typescript
+ * import express from 'express';
+ * import { addonBuilder, getRouter } from 'stremio-addon-sdk';
+ * import { manifest } from './manifest';
+ *
+ * const app = express();
+ * const port = 3000;
+ *
+ * const builder = new addonBuilder(manifest);
+ *
+ * // builder.defineCatalogHandler(...)
+ * // ...
+ *
+ * const addonInterface = builder.getInterface();
+ * const addonRouter = getRouter(addonInterface);
+ *
+ * app.use(addonRouter);
+ *
+ * app.listen(port, () => {
+ *   console.log(`Addon listening on port ${port}`);
+ * });
+ * ```
  */
 declare function getRouter(addonInterface: AddonInterface): any;
 
